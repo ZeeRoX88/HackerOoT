caf41d320cd1e38819acb33295af97dc81a74934
ZeeRoX88 <info@michael-teichmann.com>
2026-06-22 19:51:31 +0200

    crash is still happening

--- src/code/z_kankyo.c
+++ src/code/z_kankyo.c
@@ -1735,9 +1735,11 @@ void Environment_DrawSkybox(PlayState* play) {
     Skybox_DrawNew(&play->skyboxCtx, play->state.gfxCtx, &play->lightCtx, play->skyboxId, play->envCtx.skyboxBlend,
                     play->view.eye.x, play->view.eye.y, play->view.eye.z);
 
-    Environment_DrawCloudStorm(play);
-
     OPEN_DISPS(play->state.gfxCtx, __FILE__, __LINE__);
+
+    gSPSegment(POLY_XLU_DISP++, 0x7, play->skyboxCtx.skyboxStaticSegment); // setting the correct segment for xlu
+
+    Environment_DrawCloudStorm(play);
 
     static u8 fogIntensity;
     if (play->envCtx.changeSkyboxNextConfig != 0 || (play->envCtx.skyboxConfig != 0 && play->envCtx.changeSkyboxNextConfig != 0)) { // storm condition
@@ -1874,8 +1876,7 @@ void Environment_DrawCloudStorm(PlayState* play) {
 
     OPEN_DISPS(play->state.gfxCtx, __FILE__, __LINE__);
 
-    gSPSegment(POLY_XLU_DISP++, 0x7, play->skyboxCtx.skyboxStaticSegment); // setting the correct segment for xlu
-
+    gDPPipeSync(POLY_XLU_DISP++);
     gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, play->envCtx.dirLight1.params.dir.color[0], play->envCtx.dirLight1.params.dir.color[1], play->envCtx.dirLight1.params.dir.color[2], stormAlpha);
     gDPSetEnvColor(POLY_XLU_DISP++, play->envCtx.dirLight2.params.dir.color[0], play->envCtx.dirLight2.params.dir.color[1], play->envCtx.dirLight2.params.dir.color[2], 0);
 
@@ -1894,8 +1895,7 @@ void Environment_DrawCloudStorm(PlayState* play) {
 void Environment_DrawCloudHorizon(PlayState* play) {
     OPEN_DISPS(play->state.gfxCtx, __FILE__, __LINE__);
 
-    gSPSegment(POLY_XLU_DISP++, 0x7, play->skyboxCtx.skyboxStaticSegment); // setting the correct segment for xlu
-
+    gDPPipeSync(POLY_XLU_DISP++);
     gDPSetPrimColor(POLY_XLU_DISP++, 0, 0, play->envCtx.dirLight1.params.dir.color[0], play->envCtx.dirLight1.params.dir.color[1], play->envCtx.dirLight1.params.dir.color[2], 150);
     gDPSetEnvColor(POLY_XLU_DISP++, play->envCtx.dirLight2.params.dir.color[0], play->envCtx.dirLight2.params.dir.color[1], play->envCtx.dirLight2.params.dir.color[2], 0);
 
@@ -1920,8 +1920,7 @@ void Environment_DrawClouds(PlayState* play) {
 
     OPEN_DISPS(play->state.gfxCtx, "../z_cheap_proc.c", 214);
 
-    gSPSegment(POLY_XLU_DISP++, 0x7, play->skyboxCtx.skyboxStaticSegment); // setting the correct segment for xlu
-
+    gDPPipeSync(POLY_XLU_DISP++);
     gDPSetEnvColor(POLY_XLU_DISP++, play->envCtx.dirLight2.params.dir.color[0], play->envCtx.dirLight2.params.dir.color[1], play->envCtx.dirLight2.params.dir.color[2], 0);
 
     for (i = 0; i < ARRAY_COUNT(play->envCtx.clouds); i++) {
